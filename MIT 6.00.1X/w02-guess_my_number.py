6981d3223770763038c69f184513bd6cb9028ef5
Donavin <donavinhannon@gmail.com>
2017-06-18 11:50:08 -0700

    Fixed naming structure, completed guess_my_number exercise

---
+++ MIT 6.00.1X/w02-guess_my_number.py
@@ -0,0 +1,28 @@
+top = 100
+bottom = 0
+guess = 50
+guess_text = 'Is your secret number ' + str(guess) + '?'
+inn_text = "Enter 'h' to indicate the guess is too high. Enter 'l' to indicate the guess is too low. Enter 'c' to indicate I guessed correctly. "
+
+print("Please think of a number between 0 and 100!")
+print("Is your secret number 50?")
+
+inn = str(input(inn_text))
+
+while inn != 'c':
+    if inn == 'h':
+        top = guess
+        guess -= round((top - bottom) / 2)
+        print('Is your secret number ' + str(guess) + '?')
+        inn = str(input(inn_text))
+    elif inn == 'l':
+        bottom = guess
+        guess += round((top - bottom) / 2)
+        print('Is your secret number ' + str(guess) + '?')
+        inn = str(input(inn_text))
+    else:
+        print('Sorry, I did not understand your input.')
+        print('Is your secret number ' + str(guess) + '?')
+        inn = str(input(inn_text))
+
+print('Game over. Your secret number was: ' + str(guess))
