0985cb8aa816a57c809a20eb16e7b206a975a7f0
Donavin <donavinhannon@gmail.com>
2017-06-27 23:38:44 -0700

    Moved practice-python.org problems to 'org' folder

--- 05.1.py
+++
@@ -1,7 +0,0 @@
-#! /usr/bin/env python2
-# -*- coding: utf-8 -*-
-a = set([1, 1, 2, 3, 5, 8, 13, 21, 34, 55, 89])
-
-b = set([1, 2, 3, 4, 5, 6, 7, 8, 9, 10, 11, 12, 13])
-
-print("In common elements = {} ".format(a&b))
