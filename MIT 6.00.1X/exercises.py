dc62892e0e19018da90c10520bf3e15251a5f67a
Donavin <donavinhannon@gmail.com>
2017-07-06 17:51:19 -0700

    Start of problem set 4

--- MIT 6.00.1X/exercises.py
+++ MIT 6.00.1X/exercises.py
@@ -1,22 +1,13 @@
-def biggest(aDict):
-    '''
-    aDict: A dictionary, where all the values are lists.
-
-    returns: The key with the largest number of values associated with it
-    '''
-    biggest_value = 0
-    for elem in aDict.items():
-        if len(elem[1]) > biggest_value:
-            biggest_value = len(elem[1])
-            biggest_key = elem[0]
-            print(len(elem[1]))
-    return biggest_key
+def fancy_divide(list_of_numbers, index):
+    denom = list_of_numbers[index]
+    return [simple_divide(item, denom) for item in list_of_numbers]
 
 
+def simple_divide(item, denom):
+    try:
+        return item / denom
+    except ZeroDivisionError:
+        return 0
 
-animals = { 'a': ['aardvark'], 'b': ['baboon'], 'c': ['coati']}
 
-animals['d'] = ['donkey']
-animals['d'].append('dog')
-animals['d'].append('dingo')
-print(biggest(animals))
+fancy_divide([0, 2, 4], 0)
